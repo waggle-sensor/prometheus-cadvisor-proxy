dbaf870e8e01d7ca8f5f7fa22e1e9463772157e8
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-11-10 17:42:02 -0600

    update

---
+++ prometheus-tools/gen-targets.py
@@ -0,0 +1,32 @@
+import json
+import requests
+
+vsns = [
+    "W021",
+    "W026",
+    "W02C",
+    "W02E",
+    "W015",
+    "W01B",
+    "W022",
+    "W017",
+]
+
+r = requests.get("https://api.sagecontinuum.org/api/state")
+items = r.json()["data"]
+node_for_vsn = {item["vsn"].upper(): item["id"].lower() for item in items if item.get("vsn") and item.get("id")}
+
+targets = [
+    {
+        "labels": {
+            "vsn": vsn,
+            "node": node_for_vsn[vsn],
+        },
+        "targets": [
+            "localhost:9911",
+        ]
+    }
+    for vsn in vsns
+]
+
+print(json.dumps(targets, sort_keys=True))
