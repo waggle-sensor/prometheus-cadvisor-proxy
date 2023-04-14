fdb7fb5bb75c43c1c3fd57793a738d6dbdd22a3d
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-04-14 16:28:42 -0500

    clean up logs. added timeout. fixed WriteHeader error.

--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 )
+
+var timeout = flag.Duration("timeout", 30*time.Second, "timeout for scraping node metrics")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	nodeID, _, _ := strings.Cut(r.URL.Path, "/")
 	nodeID = strings.ToLower(nodeID)
 
+	log.Printf("[%s] starting metrics collection", nodeID)
+	defer log.Printf("[%s] finished metrics collection", nodeID)
+
 	// TODO(scrape all nodes, not just nxcore)
-	ctx, cancel := context.WithCancel(r.Context())
-	cmd := exec.CommandContext(ctx, "ssh", "-C", fmt.Sprintf("node-%s", nodeID), "kubectl", "get", "--raw", fmt.Sprintf("/api/v1/nodes/%s.ws-nxcore/proxy/metrics/cadvisor", nodeID))
+	ctx, cancel := context.WithTimeout(r.Context(), *timeout)
 	defer cancel()
+	cmd := exec.CommandContext(ctx, "ssh", "-C", fmt.Sprintf("node-%s", nodeID), "kubectl", "get", "--raw", fmt.Sprintf("/api/v1/nodes/%s.ws-nxcore/proxy/metrics/cadvisor", nodeID))
 
-	cmd.Stdout = w
+	b, err := cmd.CombinedOutput()
 
-	if err := cmd.Run(); err != nil {
-		log.Printf("proxy error for %s: %s", nodeID, err)
-		http.Error(w, http.StatusText(500), 500)
+	if err != nil {
+		log.Printf("[%s] error during metric collection: %s", nodeID, err)
+		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		return
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		log.Printf("[%s] error writing response: %s", nodeID, err)
 		return
 	}
 }
 
 func main() {
+	flag.Parse()
 	http.Handle("/metrics/", http.StripPrefix("/metrics/", http.HandlerFunc(handler)))
 	http.ListenAndServe(":9911", nil)
 }
--- prometheus-tools/gen-targets.py
+++ prometheus-tools/gen-targets.py
@@ -1,21 +1,46 @@
 import json
 import requests
 
-vsns = [
-    "W021",
-    "W026",
-    "W02C",
-    "W02E",
-    "W015",
-    "W01B",
-    "W022",
-    "W017",
-    "W023",
-]
+#  missing???
+
+vsns = sorted(set("""
+W02E
+W02C
+W026
+W030
+W039
+W014
+W01E
+W01B
+W018
+W024
+W021
+W023
+W028
+W019
+W022
+W02B
+W02F
+W029
+W02D
+W015
+W01C
+W016
+W073
+W06F
+W084
+W06D
+W057
+W059
+W045
+W06B
+""".split()))
 
 r = requests.get("https://api.sagecontinuum.org/api/state")
 items = r.json()["data"]
 node_for_vsn = {item["vsn"].upper(): item["id"].lower() for item in items if item.get("vsn") and item.get("id")}
+# missing???
+node_for_vsn["W06B"] = node_for_vsn.get("W06B", "000048B02D3AE349")
 
 targets = [
     {
