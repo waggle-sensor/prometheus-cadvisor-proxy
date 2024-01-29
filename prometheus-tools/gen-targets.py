cf16238b6e60bc5c79a6915bc5bce16e1bcfb85b
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-01-29 15:57:13 -0600

    get targets from manifests. include project tag. only include deployed nodes.

--- prometheus-tools/gen-targets.py
+++ prometheus-tools/gen-targets.py
@@ -1,58 +1,27 @@
 import json
 import requests
+import re
 
-#  missing???
-
-vsns = sorted(set("""
-W02E
-W02C
-W026
-W030
-W039
-W014
-W01E
-W01B
-W018
-W024
-W021
-W023
-W028
-W019
-W022
-W02B
-W02F
-W029
-W02D
-W015
-W01C
-W016
-W073
-W06F
-W084
-W06D
-W057
-W059
-W045
-W06B
-""".split()))
-
-r = requests.get("https://api.sagecontinuum.org/api/state")
-items = r.json()["data"]
-node_for_vsn = {item["vsn"].upper(): item["id"].lower() for item in items if item.get("vsn") and item.get("id")}
-# missing???
-node_for_vsn["W06B"] = node_for_vsn.get("W06B", "000048B02D3AE349")
+r = requests.get("https://auth.sagecontinuum.org/manifests/")
+r.raise_for_status()
+items = r.json()
+items.sort(key=lambda item: item["vsn"])
 
 targets = [
     {
         "labels": {
-            "vsn": vsn,
-            "node": node_for_vsn[vsn],
+            "vsn": item["vsn"].upper(),
+            "node": item["name"].lower(),
+            "project": (item.get("project") or "").lower(),
         },
         "targets": [
             "localhost:9911",
-        ]
+        ],
     }
-    for vsn in vsns
+    for item in items
+    if item["name"]
+    and item["phase"] == "Deployed"
+    and re.match(r"^W[0-9A-Z]+$", item["vsn"])
 ]
 
 print(json.dumps(targets, sort_keys=True))
